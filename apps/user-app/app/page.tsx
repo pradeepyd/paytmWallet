083644d0e243e8485b60952497e52dbe07a07fc0
Pradeep yadav <pradeep20020102@gmail.com>
2025-01-22 23:37:02 +0545

    user app side backend initialized

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,12 @@
+import Image, { type ImageProps } from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+
+export default function Home() {
+  return (
+    <div className="text-3xl">
+      hello there
+    </div>
+  );
+}
