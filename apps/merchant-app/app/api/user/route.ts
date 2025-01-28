6889278cb55b7f5cb0bb915d5c258047625f7dd2
Pradeep yadav <pradeep20020102@gmail.com>
2025-01-28 23:40:23 +0545

    user route created

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,13 +1,14 @@
 import { NextResponse } from "next/server"
-import { PrismaClient } from "@repo/db/client";
+import db from "@repo/db/client";
 
-const client = new PrismaClient();
+// const client = new PrismaClient();
 
 export const GET = async () => {
-    await client.user.create({
-        data: {
+    await db.merchant.create({
+        data : {
             email: "asd",
-            name: "adsads"
+            name: "adsads",
+            auth_type:"Google"
         }
     })
     return NextResponse.json({
